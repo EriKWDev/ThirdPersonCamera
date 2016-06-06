2640931ee9ec8151456af37a8985c242b1d50b80
EriKWDev <ErikWDev@gmail.com>
2016-06-06 08:39:21 +0200

    First commit - All files from local project
    Summary says it all...

---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("ErikWDev/Third Person Camera Follow Script")]
+[DisallowMultipleComponent]
+public class ThirdPersonCamera : MonoBehaviour {
+
+	[Header("Debug")]
+	public bool debug = false;
+
+	[Header("Private Values")]
+	[SerializeField]
+	private float distanceAway;
+	[SerializeField]
+	private float distanceUp;
+	[SerializeField]
+	private float smooth;
+
+	[SerializeField]
+	private Transform follow;
+	private Vector3 targetPosition;
+
+	//[Header("Public Values")]
+
+	void Start () {
+		follow = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+
+	void Update () {
+	
+	}
+
+	void OnDrawGizmos () {
+		
+	}
+
+	void LateUpdate() {
+		targetPosition = follow.position + follow.up * distanceUp - follow.forward * distanceAway;
+
+		if (debug) {
+			Debug.DrawRay (follow.position, Vector3.up * distanceUp, Color.red);
+			Debug.DrawRay (follow.position, -1f * follow.forward * distanceAway, Color.blue);
+			Debug.DrawLine (follow.position, targetPosition, Color.magenta);;
+		}
+
+		transform.position = Vector3.Lerp (transform.position, targetPosition, Time.deltaTime * smooth);
+		transform.LookAt (follow);
+	}
+}
---
+++ Assets/Scripts/ThirdPersonPlayer.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonPlayer : MonoBehaviour {
+
+	[Header("Debug")]
+	public bool debug = false;
+
+	[Header("Private Values")]
+	[SerializeField]
+	private ThirdPersonCamera gamecam;
+	[SerializeField]
+	private float directionSpeed = 3f;
+
+	private float speed = 0f;
+	private float direction = 0f;
+	private float h = 0f;
+	private float v = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		h = Input.GetAxis ("Horizontal");
+		v = Input.GetAxis ("Vertical");
+
+		speed = new Vector2 (h, v).sqrMagnitude;
+
+		StickToWorldSpace (this.transform, gamecam.transform, ref direction, ref speed);
+	}
+
+	public void StickToWorldSpace(Transform root, Transform camera, ref float directionOut, ref float speedOut) {
+		Vector3 rootDirection = root.forward;
+
+		Vector3 stickDirection = new Vector3 (h, 0f, v);
+
+		speedOut = stickDirection.sqrMagnitude;
+
+		Vector3 cameraDirection = camera.forward;
+		cameraDirection.y = 0f;
+		Quaternion referentialShift = Quaternion.FromToRotation (Vector3.forward, rootDirection);
+
+		Vector3 moveDirection = referentialShift * stickDirection;
+		Vector3 axisSign = Vector3.Cross (moveDirection, rootDirection);
+
+		if (debug) {
+			Debug.DrawRay (new Vector3 (root.position.x, root.position.y + 2f, root.position.z), moveDirection, Color.green);
+			Debug.DrawRay (new Vector3 (root.position.x, root.position.y + 2f, root.position.z), axisSign, Color.red);
+			Debug.DrawRay (new Vector3 (root.position.x, root.position.y + 2f, root.position.z), rootDirection, Color.magenta);
+			Debug.DrawRay (new Vector3 (root.position.x, root.position.y + 2f, root.position.z), stickDirection, Color.blue);
+		}
+	}
+}
